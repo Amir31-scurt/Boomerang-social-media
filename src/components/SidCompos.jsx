8e20c36f115b068b2dffb49e57c0f5bd2a02698d
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2023-12-07 09:13:29 +0000

    bug fix

--- src/components/SidCompos.jsx
+++ src/components/SidCompos.jsx
@@ -65,7 +65,7 @@ export default function SideBar() {
   const SideMenu = () => {
     return SideElem.map((item, index) => {
       return (
-        <li className="nav-item">
+        <li className="">
           <Link
             className="d-flex align-items-center justify-content-center justify-content-lg-start gap-3 link"
             to={item.link}
@@ -81,14 +81,43 @@ export default function SideBar() {
   };
   // affichage //////////////
   return (
-    <nav class="navigation mx-1 mx-lg-4 scroll-bar">
-      <div class="container ps-0 pe-0">
-        <div class="nav-content">
-          <div class="nav-wrap bg-secondary bg-opacity-25 p-3 rounded rounded-3 shadow-sm pt-3 pb-1 mb-2 mt-2">
-            <ul class="mb-1 top-content list-unstyled">{SideMenu()}</ul>
-          </div>
-        </div>
+    <div class="nav-wrap bg-white bg-transparent-card rounded-xxl shadow-xss pt-3 pb-1 mb-2 mt-2">
+      <div class="nav-caption fw-600 font-xssss text-grey-500">
+        <span>New </span>Feeds
       </div>
-    </nav>
+      <ul class="mb-1 top-content">
+        <li class="logo d-none d-xl-block d-lg-block"></li>
+        <li>
+          <a href="default.html" class="nav-content-bttn open-font">
+            <i class="feather-tv btn-round-md bg-blue-gradiant me-3"></i>
+            <span>Newsfeed</span>
+          </a>
+        </li>
+        <li>
+          <a href="default-badge.html" class="nav-content-bttn open-font">
+            <i class="feather-award btn-round-md bg-red-gradiant me-3"></i>
+            <span>Badges</span>
+          </a>
+        </li>
+        <li>
+          <a href="default-storie.html" class="nav-content-bttn open-font">
+            <i class="feather-globe btn-round-md bg-gold-gradiant me-3"></i>
+            <span>Explore Stories</span>
+          </a>
+        </li>
+        <li>
+          <a href="default-group.html" class="nav-content-bttn open-font">
+            <i class="feather-zap btn-round-md bg-mini-gradiant me-3"></i>
+            <span>Popular Groups</span>
+          </a>
+        </li>
+        <li>
+          <a href="user-page.html" class="nav-content-bttn open-font">
+            <i class="feather-user btn-round-md bg-primary-gradiant me-3"></i>
+            <span>Author Profile </span>
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
--- src/layouts/Template.jsx
+++ src/layouts/Template.jsx
@@ -5,7 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import React, { useContext } from 'react';
 import { AuthContext } from '../contexte/authContext';
-import { Outlet, useLocation, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import NavBar from '../components/NavCompos';
 import SideBar from '../components/SidCompos';
 import Home from '../pages/Home';
@@ -27,11 +27,11 @@ export default function Template() {
         <div className="col-12 border-gray-300 NavBar">
           <NavBar />
         </div>
-        <div className="SideAndContainer d-flex row m-0 p-0">
-          <div className="col-2 col-lg-3">
+        <div className="SideAndContainer d-flex">
+          <div className="col-2 col-lg-2">
             <SideBar />
           </div>
-          <div className="col-10 col-lg-9 bg-secondary bg-opacity-25">
+          <div className="col-10 col-lg-10 bg-secondary bg-opacity-25">
             {/* Place ////////////////l */}
             <Home />
           </div>

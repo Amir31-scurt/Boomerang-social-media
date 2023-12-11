be064325bf1840a88ef01d5b5f56cfcb2c623ce9
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2023-12-11 02:47:07 +0000

    Import UserPage

--- src/components/AutreProfile/AutreProfile.jsx
+++ src/components/AutreProfile/AutreProfile.jsx
@@ -1,10 +1,9 @@
-
-import React, { useState } from "react";
-import { IoLinkOutline } from "react-icons/io5";
-import { BiMessageDots } from "react-icons/bi";
-import { BsFillGridFill } from "react-icons/bs";
-import { MdOutlinePlayCircleFilled } from "react-icons/md";
-import { BiSolidUserPin } from "react-icons/bi";
+import React, { useState } from 'react';
+import { IoLinkOutline } from 'react-icons/io5';
+import { BiMessageDots } from 'react-icons/bi';
+import { BsFillGridFill } from 'react-icons/bs';
+import { MdOutlinePlayCircleFilled } from 'react-icons/md';
+import { BiSolidUserPin } from 'react-icons/bi';
 
 const ContenuA = () => (
   <div className="container py-4 rounded-3 imagespublié mt-3 ">
@@ -69,8 +68,8 @@ const ContenuA = () => (
 const ContenuB = () => <div>Contenu B</div>;
 const ContenuC = () => <div>Contenu C</div>;
 
-const AutreProfile= () => {
-  const [contenuId, setContenuId] = useState("contenuA");
+const AutreProfile = () => {
+  const [contenuId, setContenuId] = useState('contenuA');
 
   const afficherContenu = (id) => {
     setContenuId(id);
@@ -78,12 +77,13 @@ const AutreProfile= () => {
 
   return (
     <div className="container">
-      <div className="position-relative bg-white w-100 justify-content-center align-items-center blanc  rounded-3 mt-5 p-4 shadow-lg">
+      <div className="position-relative bg-white w-100 justify-content-center align-items-center blanc  rounded-3 mt-5 p-4">
         <div className="">
           <div className="plan bg-white  p-4">
             <img
               src="https://cdn.pixabay.com/photo/2020/12/26/13/19/christmas-background-5861438_640.jpg"
               className="w-100 haut rounded-4 img-fluid"
+              alt=""
             />
           </div>
           <div>
@@ -133,7 +133,7 @@ const AutreProfile= () => {
         <button
           type="button"
           className="btn d-flex align-items-center  btn-info text-white rounded-5"
-          onClick={() => afficherContenu("contenuA")}
+          onClick={() => afficherContenu('contenuA')}
         >
           <div className="pe-3">
             <BsFillGridFill />
@@ -143,7 +143,7 @@ const AutreProfile= () => {
         <button
           type="button"
           className="btn d-flex align-items-center  btn-info text-white rounded-5 active"
-          onClick={() => afficherContenu("contenuB")}
+          onClick={() => afficherContenu('contenuB')}
         >
           <div className="pe-3">
             <MdOutlinePlayCircleFilled />
@@ -153,7 +153,7 @@ const AutreProfile= () => {
         <button
           type="button"
           className="btn d-flex align-items-center btn-info text-white  rounded-5"
-          onClick={() => afficherContenu("contenuC")}
+          onClick={() => afficherContenu('contenuC')}
         >
           <div className="pe-3">
             <BiSolidUserPin />
@@ -161,9 +161,9 @@ const AutreProfile= () => {
           Mots clés
         </button>
       </div>
-      {contenuId === "contenuA" && <ContenuA />}
-      {contenuId === "contenuB" && <ContenuB />}
-      {contenuId === "contenuC" && <ContenuC />}
+      {contenuId === 'contenuA' && <ContenuA />}
+      {contenuId === 'contenuB' && <ContenuB />}
+      {contenuId === 'contenuC' && <ContenuC />}
     </div>
   );
 };

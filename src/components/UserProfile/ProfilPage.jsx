2523dd6d7ef976eeee299082b1ccf3a4a1d719b8
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2023-12-15 12:55:37 +0000

    WIP : UserPage

--- src/components/UserProfile/ProfilPage.jsx
+++ src/components/UserProfile/ProfilPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { Banner } from './Banner'
-import Profile from './Profile'
+import React, { useState, useEffect, useContext } from 'react';
+import { Banner } from './Banner';
+import Profile from './Profile';
 import { CgMore } from 'react-icons/cg';
 import { PostImageProfile } from './PostImageProfile';
 import PostVideoProfile from './PostVideoProfile';
@@ -9,13 +9,16 @@ import '../../assets/css/Profile.css';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { v4 as uuidv4 } from 'uuid';
 import { storage } from '../../config/firebase-config.js';
-
+import { AuthContext } from '../../contexte/authContext.js';
 
 function ProfilPage() {
+  const { user, currentUser } = useContext(AuthContext);
 
-  {/*// const [profileImageUrl, setProfileImageUrl] = useState(
+  {
+    /*// const [profileImageUrl, setProfileImageUrl] = useState(
   //   'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-// );*/}
+// );*/
+  }
 
   // Utilisation de useState pour gérer l'URL de l'image de profil
   const [profileImageUrl, setProfileImageUrl] = useState(() => {
@@ -26,7 +29,6 @@ function ProfilPage() {
 
   // Utilisation de useState pour gérer l'onglet actif par défaut
   const [activeTab, setActiveTab] = useState('images');
-
 
   // Utilisation de useEffect pour sauvegarder l'URL de l'image dans le localStorage à chaque changement
   useEffect(() => {
@@ -63,29 +65,56 @@ function ProfilPage() {
   };
 
   return (
-    <div className='ProfilPage mt-4'>
+    <div className="ProfilPage mt-4">
       <div className="container">
         <div className="bg-white shadow-sm rounded-4 page-profil">
           <div className="Profil-img p-3">
-            <Banner />
+            <div className="container my-3">
+              <div className="rounded-5 bg-white">
+                <input
+                  src={currentUser.Banner}
+                  alt=""
+                  className="img-fluid rounded-5 w-100 banner"
+                />
+              </div>
+            </div>
           </div>
           <div className=" d-flex justify-content-between pb-4 mx-5 identite">
             <div className="d-flex this-text-profil">
               <div className="profil-img rounded-5 cursor-pointer">
-                <Profile imageUrl={profileImageUrl} className="profil-img" />
+                <Profile
+                  imageUrl={currentUser.photoURL}
+                  className="profil-img"
+                />
               </div>
               <div className="text-profil">
-                <h5 className=" fw-bold text-black">Becky Lynch</h5>
-                <p className="text-sm ttext-[#145DA0] font-bold bg-gray-200 text-2xl rounded-full">example@gmail.com</p>
+                <h5 className=" fw-bold text-black">
+                  {currentUser.displayName}
+                </h5>
+                <p className="text-sm ttext-[#145DA0] font-bold bg-gray-200 text-2xl rounded-full">
+                  {currentUser.email}
+                </p>
               </div>
             </div>
             <div className="btn-profil d-flex align-items-center Btn-Group">
-              <input type="file" accept="image/*" className="d-none" id="profileImageInput" onChange={handleChangeProfileImage} />
-              <button htmlFor="profileImageInput" className="btn btn-primary btn-sm me-2 w-100 rounded-5">
+              <input
+                type="file"
+                accept="image/*"
+                className="d-none"
+                id="profileImageInput"
+                onChange={handleChangeProfileImage}
+              />
+              <label
+                htmlFor="profileImageInput"
+                className="btn btn-primary btn-sm me-2 w-100 rounded-pill"
+              >
                 modifier le profil
-              </button>
-              <button type="button" className="btn btn-primary btn-sm rounded-5 w-75">
-                <CgMore className='fs-2' />
+              </label>
+              <button
+                type="button"
+                className="btn btn-primary btn-sm rounded-5 w-75"
+              >
+                <CgMore className="fs-2" />
               </button>
             </div>
           </div>
@@ -95,18 +124,31 @@ function ProfilPage() {
           <div className="Publications p-3">
             <ul className="d-flex justify-content-around align-items-center list-unstyled">
               <li>
-                <button className={`border-0 ${activeTab === 'images' ? 'act' : ''}`} onClick={() => handleTabClick('images')}>
+                <button
+                  className={`border-0 ${activeTab === 'images' ? 'act' : ''}`}
+                  onClick={() => handleTabClick('images')}
+                >
                   Images
                 </button>
               </li>
 
               <li>
-                <button className={`rounded-5 border-0 ${activeTab === 'videos' ? 'act' : ''}`} onClick={() => handleTabClick('videos')}>
+                <button
+                  className={`rounded-5 border-0 ${
+                    activeTab === 'videos' ? 'act' : ''
+                  }`}
+                  onClick={() => handleTabClick('videos')}
+                >
                   Videos
                 </button>
               </li>
               <li>
-                <button className={`rounded-5 border-0 ${activeTab === 'posts' ? 'act' : ''}`} onClick={() => handleTabClick('posts')}>
+                <button
+                  className={`rounded-5 border-0 ${
+                    activeTab === 'posts' ? 'act' : ''
+                  }`}
+                  onClick={() => handleTabClick('posts')}
+                >
                   Posts
                 </button>
               </li>
@@ -120,7 +162,7 @@ function ProfilPage() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ProfilPage
+export default ProfilPage;

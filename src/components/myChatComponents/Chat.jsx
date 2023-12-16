90b3704d57c8f0c9782c7b3ec078a1c0de8dfbbb
Sam Laye <layesamss97@gmail.com>
2023-12-16 15:02:33 +0000

    CREATE_Chat_Sidebar_MyChatHome-components

---
+++ src/components/myChatComponents/Chat.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import user from "../images/user.png";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import Messages from "./Messages";
+import Input from "./Input";
+
+export default function Chat() {
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Nom du user</span>
+        <div className="chatIcons">
+          <i className="bi bi-camera-video-fill"></i>
+          <i className="bi bi-person-fill-add"></i>
+          <i className="bi bi-three-dots"></i>
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
+}

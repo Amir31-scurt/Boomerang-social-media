61aa6a3427484665fd3de216ab5e20494787d834
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2023-12-18 00:39:32 +0000

    Nice

--- src/components/composSignUp/SignUp.jsx
+++ src/components/composSignUp/SignUp.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ClipLoader } from 'react-spinners';
-import icon from '../../assets/images/User.png';
+import icon from '../../assets/images/user.png';
 import banner from '../../assets/images/Banner.jpg';
 
 function SignUp() {

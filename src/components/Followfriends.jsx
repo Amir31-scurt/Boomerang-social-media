f296c58bef6ab9a5f57a90be60c549107bbc26a3
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2023-12-18 01:09:41 +0000

    Merge Mariama's Branch
    Merge Mariama's Branch

--- src/components/Followfriends.jsx
+++ src/components/Followfriends.jsx
@@ -1,61 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Link } from 'react-router-dom';
 import { DB } from '../config/firebase-config';
-import { collection, where,query, getDoc, getDocs,doc } from 'firebase/firestore';
-import { useState,useEffect } from 'react';
-
+import { collection, where, query, getDocs } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { AuthContext } from '../contexte/authContext';
 
 export default function Followfriends() {
   const [following, setFollowing] = useState([]);
   const [followers, setFollowers] = useState([]);
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
-    const fetchFollowing = async (userUid) => {
-      const followingRef = doc(DB,'users',userUid);
+    const fetchFollowing = async () => {
+      // Reference the collection, not a single document
+      const followingRef = collection(DB, 'users');
       const followingQuery = query(
-        followingRef,userUid &&
-        where('following', 'array-contains', userUid)
+        followingRef,
+        where('following', 'array-contains', currentUser.uid)
       );
-      const followingSnapshot = await getDoc(followingQuery);
+      const followingSnapshot = await getDocs(followingQuery);
 
       const followingList = followingSnapshot.docs.map((doc) => doc.data());
       setFollowing(followingList);
     };
-    const fetchFollowers = async (userUid) => {
-      const followersRef = doc(DB, 'users',userUid);
-       const followersQuery = query(
-         followersRef,userUid &&
-        where('followers', 'array-contains', userUid)
-       );
-      const followersSnapshot = await getDoc(followersQuery);
+
+    const fetchFollowers = async () => {
+      // Reference the collection, not a single document
+      const followersRef = collection(DB, 'users');
+      const followersQuery = query(
+        followersRef,
+        where('followers', 'array-contains', currentUser.uid)
+      );
+      const followersSnapshot = await getDocs(followersQuery);
 
       const followersList = followersSnapshot.docs.map((doc) => doc.data());
       setFollowers(followersList);
     };
 
-    fetchFollowers();
-    fetchFollowing();
+    // You need to pass userUid to these functions
+    const userUid = currentUser.uid; // Replace this with the actual user UID
+    fetchFollowers(userUid);
+    fetchFollowing(userUid);
   }, []);
+
   return (
-    <div className="FollowCard mt-5">
+    <div className="FollowCard">
       <div class="card-body">
-        <ul className=''>
+        <ul className="">
           <h4 className="text-start">Amis que j'ai suivie</h4>
           <hr />
           {following.map((followingList) => (
-          <li key={followingList.uid}>{followingList.displayName}</li>
-        ))}
+            <li key={followingList.uid}>{followingList.displayName}</li>
+          ))}
         </ul>
-         <ul>
+        <ul>
           <h4 className="text-start">Amis</h4>
           <hr />
           {followers.map((user) => (
-          <li key={user.uid}>{user.displayName}</li>
-        ))}
-        </ul> 
+            <li key={user.uid}>{user.displayName}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
